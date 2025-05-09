1d273cac4425c799af76f1eec1bb68782af048f8
sangam14 <smbiradar14@gmail.com>
2025-05-09 22:18:23 +0530

    add

--- node_modules/@sveltejs/kit/src/runtime/server/index.js
+++ node_modules/@sveltejs/kit/src/runtime/server/index.js
@@ -1,8 +1,22 @@
 import { respond } from './respond.js';
-import { set_private_env, set_public_env } from '../shared-server.js';
+import { set_private_env, set_public_env, set_safe_public_env } from '../shared-server.js';
 import { options, get_hooks } from '__SERVER__/internal.js';
 import { DEV } from 'esm-env';
 import { filter_private_env, filter_public_env } from '../../utils/env.js';
+import { prerendering } from '__sveltekit/environment';
+import { set_read_implementation, set_manifest } from '__sveltekit/server';
+
+/** @type {ProxyHandler<{ type: 'public' | 'private' }>} */
+const prerender_env_handler = {
+	get({ type }, prop) {
+		throw new Error(
+			`Cannot read values from $env/dynamic/${type} while prerendering (attempted to read env.${prop.toString()}). Use $env/static/${type} instead`
+		);
+	}
+};
+
+/** @type {Promise<any>} */
+let init_promise;
 
 export class Server {
 	/** @type {import('types').SSROptions} */
@@ -16,40 +30,59 @@ export class Server {
 		/** @type {import('types').SSROptions} */
 		this.#options = options;
 		this.#manifest = manifest;
+
+		set_manifest(manifest);
 	}
 
 	/**
 	 * @param {{
-	 *   env: Record<string, string>
+	 *   env: Record<string, string>;
+	 *   read?: (file: string) => ReadableStream;
 	 * }} opts
 	 */
-	async init({ env }) {
+	async init({ env, read }) {
 		// Take care: Some adapters may have to call `Server.init` per-request to set env vars,
 		// so anything that shouldn't be rerun should be wrapped in an `if` block to make sure it hasn't
 		// been done already.
+
 		// set env, in case it's used in initialisation
+		const prefixes = {
+			public_prefix: this.#options.env_public_prefix,
+			private_prefix: this.#options.env_private_prefix
+		};
+
+		const private_env = filter_private_env(env, prefixes);
+		const public_env = filter_public_env(env, prefixes);
+
 		set_private_env(
-			filter_private_env(env, {
-				public_prefix: this.#options.env_public_prefix,
-				private_prefix: this.#options.env_private_prefix
-			})
+			prerendering ? new Proxy({ type: 'private' }, prerender_env_handler) : private_env
 		);
 		set_public_env(
-			filter_public_env(env, {
-				public_prefix: this.#options.env_public_prefix,
-				private_prefix: this.#options.env_private_prefix
-			})
+			prerendering ? new Proxy({ type: 'public' }, prerender_env_handler) : public_env
 		);
+		set_safe_public_env(public_env);
 
-		if (!this.#options.hooks) {
+		if (read) {
+			set_read_implementation(read);
+		}
+
+		// During DEV and for some adapters this function might be called in quick succession,
+		// so we need to make sure we're not invoking this logic (most notably the init hook) multiple times
+		await (init_promise ??= (async () => {
 			try {
 				const module = await get_hooks();
 
 				this.#options.hooks = {
 					handle: module.handle || (({ event, resolve }) => resolve(event)),
 					handleError: module.handleError || (({ error }) => console.error(error)),
-					handleFetch: module.handleFetch || (({ request, fetch }) => fetch(request))
+					handleFetch: module.handleFetch || (({ request, fetch }) => fetch(request)),
+					reroute: module.reroute || (() => {}),
+					transport: module.transport || {}
 				};
+
+				if (module.init) {
+					await module.init();
+				}
 			} catch (error) {
 				if (DEV) {
 					this.#options.hooks = {
@@ -57,13 +90,15 @@ export class Server {
 							throw error;
 						},
 						handleError: ({ error }) => console.error(error),
-						handleFetch: ({ request, fetch }) => fetch(request)
+						handleFetch: ({ request, fetch }) => fetch(request),
+						reroute: () => {},
+						transport: {}
 					};
 				} else {
 					throw error;
 				}
 			}
-		}
+		})());
 	}
 
 	/**
@@ -71,13 +106,6 @@ export class Server {
 	 * @param {import('types').RequestOptions} options
 	 */
 	async respond(request, options) {
-		// TODO this should probably have been removed for 1.0 — i think we can get rid of it?
-		if (!(request instanceof Request)) {
-			throw new Error(
-				'The first argument to server.respond must be a Request object. See https://github.com/sveltejs/kit/pull/3384 for details'
-			);
-		}
-
 		return respond(request, this.#options, this.#manifest, {
 			...options,
 			error: false,
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,18 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		// For GitHub Pages deployment
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/vigilia.github.io' : ''
+		}
 	}
 };
 
